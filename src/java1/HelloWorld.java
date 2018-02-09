025f869056d91df1a229625e052a00131ee79936
DI388623 <DI388623@D-113135351.wipro.com>
2018-02-09 11:58:32 +0530

    ya

--- src/java1/HelloWorld.java
+++ src/java1/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println(" Welcome to the World");
+		System.out.println(" Welcome to 2.0 the New World");
 
 	}
 
